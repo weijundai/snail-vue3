ebb7a5217bb3853cf3a9e0dd1573df99415a5a99
weijundai <74532078+weijundai@users.noreply.github.com>
2023-04-12 20:28:33 +0800

    2 files modified

--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,26 @@
-import { createRouter,createWebHashHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from 'vue-router';
 
 const routes = [
-    {
-        path:'/login',
-        name:'login',
-        component:()=>import('@/views/login/index.vue')
-        
-    },
-    {
-        path:'/home',
-        name:'home',
-        component:()=>import('@/views/home/index.vue')
-    },
-    {
-        path:'/hello',
-        name:'hello',
-        component:()=>import('@/components/HelloWorld.vue')
-    }
-
-]
+  {
+    path: '/login',
+    name: 'login',
+    component: () => import('@/views/login/index.vue'),
+  },
+  {
+    path: '/home',
+    name: 'home',
+    component: () => import('@/views/home/index.vue'),
+  },
+  {
+    path: '/hello',
+    name: 'hello',
+    component: () => import('@/components/HelloWorld.vue'),
+  },
+];
 
 const router = createRouter({
-    history:createWebHashHistory(),
-    routes
-}
-)
+  history: createWebHashHistory(),
+  routes,
+});
 
 export default router;
